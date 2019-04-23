95c2327e9d91d12a0bc530fe3c035d97b6f950df
RolyPoly1337 <dany.nabil@gmail.com>
2019-04-24 06:24:17 +0800

    Smoothed out A.I enemy following script

--- Assets/Scripts/EnemyFollowing.cs
+++ Assets/Scripts/EnemyFollowing.cs
@@ -6,18 +6,32 @@ public class EnemyFollowing : MonoBehaviour {
     public float speed;
     private Transform whoIsBeingFollowed;
     public float stopDistance;
-	// Use this for initialization
-	void Start () {
+    private Enemy daze;
+    // Use this for initialization
+    void Start () {
         whoIsBeingFollowed = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-	}
+        daze = gameObject.GetComponent<Enemy>();
+        //speed = daze.speed;
+    }
 	
 	// Update is called once per frame
-	void Update () {
-        if(Vector2.Distance(transform.position, whoIsBeingFollowed.position) > stopDistance){
+	void FixedUpdate () {
+        speed = daze.speed;
+        if (Vector2.Distance(transform.position, whoIsBeingFollowed.position) > stopDistance){
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(whoIsBeingFollowed.position.x, transform.position.y, whoIsBeingFollowed.position.z), speed * Time.deltaTime);
+            transform.position.Normalize();
+           // speed = daze.speed;
+
+        
+        }
+        if (daze.Dazed == true)
+        {
+           speed = 0;
 
         }
+   
 
-      }
+
+    }
 
 }
