76102ebf8cd928efa97ff55a03ffcec4d7218175
RolyPoly1337 <dany.nabil@gmail.com>
2019-04-24 03:01:17 +0800

    Added basic a.i for enemy (Follows according to X-axis)

---
+++ Assets/Scripts/EnemyFollowing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowing : MonoBehaviour {
+    public float speed;
+    private Transform whoIsBeingFollowed;
+	// Use this for initialization
+	void Start () {
+        whoIsBeingFollowed = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        transform.position = Vector3.MoveTowards(transform.position,new Vector3(whoIsBeingFollowed.position.x, transform.position.y,whoIsBeingFollowed.position.z),speed*Time.deltaTime);
+	}
+}
