1bd7ffa898173a054ebe6767e41c32c15360b068
RolyPoly1337 <dany.nabil@gmail.com>
2019-04-27 22:19:16 +0800

    Added Map Version 5 - Toggle Map Using M

---
+++ Assets/ToggleOpacityMap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleOpacityMap : MonoBehaviour
+{
+    public GameObject UItoDisable;
+    public bool Toggled = false;
+    void Update()
+    {
+        if ((Input.GetKey(KeyCode.M)))
+        {
+
+            UItoDisable.GetComponent<CanvasGroup>().alpha = 1;
+            Toggled = true;
+
+        }
+        else 
+        {
+            Toggled = false;
+            UItoDisable.GetComponent<CanvasGroup>().alpha = 0;
+
+
+        }
+    }
+
+}
