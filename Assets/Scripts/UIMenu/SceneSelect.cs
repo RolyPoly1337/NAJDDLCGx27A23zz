dc5065bd9e23658360d4ed73bf636cf0be031dd9
RolyPoly1337 <dany.nabil@gmail.com>
2019-05-07 03:29:00 +0800

    Created Basic Main Menu

---
+++ Assets/Scripts/UIMenu/SceneSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelect : MonoBehaviour {
+
+    public void selectScene()
+    { switch (this.gameObject.name)
+        {
+            case "LevelSelectButton":
+                SceneManager.LoadScene("LevelSelect");
+                break;
+            case "TutorialButton":
+                SceneManager.LoadScene("SampleScene");
+                break;
+            case "Stage1Button":
+                SceneManager.LoadScene("Stage1");
+                break;
+            case "Stage1-1Button":
+                SceneManager.LoadScene("Stage1-1");
+                break;
+        }
+
+    }
+}
